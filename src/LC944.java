35a9dc082410f435a633fdae5880da8d7b7c0b34
prasanna <tech.prasanna.kulkarni@gmail.com>
2023-01-03 09:26:27 +0530

    2d is always a bit confusing

---
+++ src/LC944.java
@@ -0,0 +1,30 @@
+public class LC944 {
+    public static void main(String[] args) {
+        System.out.println(minDeletionSize(new String[]{"cba","daf","ghi"}));
+        System.out.println(minDeletionSize(new String[]{"a","b"}));
+        System.out.println(minDeletionSize(new String[]{"zyx","wvu","tsr"}));
+    }    
+
+    public static int minDeletionSize(String[] strs) {
+        int ret = 0;
+        int rowlen = strs.length;
+        int collen = strs[0].length();
+
+        char[][] grid = new char[rowlen][collen];
+        for(int i =0; i<strs.length; i++){
+            String s = strs[i];
+            for(int j =0; j<s.length(); j++){
+                grid[i][j] = s.charAt(j);
+            }
+        }
+        for(int col=0; col<collen; col++){
+            for(int row=1; row<rowlen; row++){
+                if(grid[row][col] < grid[row-1][col]){
+                    ret++;
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+}
